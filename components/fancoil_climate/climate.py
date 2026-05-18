cc35ddae171cf175f3a52f7609cc6a1c41f050df
apra00 <76562795+apra00@users.noreply.github.com>
2026-05-18 16:22:05 +0200

    Update climate.py

--- components/fancoil_climate/climate.py
+++ components/fancoil_climate/climate.py
@@ -1,8 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
-from esphome import automation
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_LAMBDA
 
 AUTO_LOAD = ["climate"]
 
@@ -12,24 +11,14 @@ FancoilClimate = fancoil_climate_ns.class_(
     "FancoilClimate", climate.Climate, cg.Component
 )
 
-FancoilControlTrigger = fancoil_climate_ns.class_(
-    "FancoilControlTrigger",
-    automation.Trigger.template(climate.ClimateCall.operator("ref")),
-)
-
-CONF_ON_CONTROL = "on_control"
-CONF_FANCOIL_TRIGGER_ID = "fancoil_trigger_id"
+# ClimateCall reference type used as the lambda parameter
+climate_call_ref = climate.ClimateCall.operator("ref")
 
 CONFIG_SCHEMA = climate._CLIMATE_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(FancoilClimate),
-        cv.Optional(CONF_ON_CONTROL): automation.validate_automation(
-            {
-                cv.GenerateID(CONF_FANCOIL_TRIGGER_ID): cv.declare_id(
-                    FancoilControlTrigger
-                ),
-            }
-        ),
+        # User writes a lambda: block; receives `call` (ClimateCall &)
+        cv.Optional(CONF_LAMBDA): cv.returning_lambda,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -39,12 +28,12 @@ async def to_code(config):
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
 
-    for conf in config.get(CONF_ON_CONTROL, []):
-        trigger = cg.new_Pvariable(conf[CONF_FANCOIL_TRIGGER_ID], var)
-        # Register the trigger with the component so control() can fire it
-        cg.add(var.add_control_trigger(trigger))
-        await automation.build_automation(
-            trigger,
-            [(climate.ClimateCall.operator("ref"), "call")],
-            conf,
+    if CONF_LAMBDA in config:
+        # process_lambda compiles the YAML lambda body into a C++ lambda.
+        # Parameters: [(type, name), ...]  -- `call` available inside the body.
+        control_lambda = await cg.process_lambda(
+            config[CONF_LAMBDA],
+            [(climate_call_ref, "call")],
+            return_type=cg.void,
         )
+        cg.add(var.set_control_callback(control_lambda))
