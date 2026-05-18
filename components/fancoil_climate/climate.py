90626c1cc9fd0a170fc5debf31471e0f54957c9a
apra00 <76562795+apra00@users.noreply.github.com>
2026-05-18 15:54:31 +0200

    Update climate.py

--- components/fancoil_climate/climate.py
+++ components/fancoil_climate/climate.py
@@ -3,13 +3,14 @@ import esphome.config_validation as cv
 from esphome.components import climate
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ["climate"]
 AUTO_LOAD = ["climate"]
 
 fancoil_climate_ns = cg.esphome_ns.namespace("fancoil_climate")
-FancoilClimate = fancoil_climate_ns.class_("FancoilClimate", climate.Climate, cg.Component)
+FancoilClimate = fancoil_climate_ns.class_(
+    "FancoilClimate", climate.Climate, cg.Component
+)
 
-CONFIG_SCHEMA = climate._CLIMATE_SCHEMA.extend(
+CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(FancoilClimate),
     }
