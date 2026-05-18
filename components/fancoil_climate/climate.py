cf79141d6b81872faf54bccbb3840aae772ad83b
apra00 <76562795+apra00@users.noreply.github.com>
2026-05-18 16:26:35 +0200

    Update climate.py

--- components/fancoil_climate/climate.py
+++ components/fancoil_climate/climate.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
-from esphome.const import CONF_ID, CONF_LAMBDA
+from esphome.const import CONF_ID
 
 AUTO_LOAD = ["climate"]
 
@@ -11,15 +11,10 @@ FancoilClimate = fancoil_climate_ns.class_(
     "FancoilClimate", climate.Climate, cg.Component
 )
 
-# ClimateCall reference type used as the lambda parameter
-climate_call_ref = climate.ClimateCall.operator("ref")
-
-CONFIG_SCHEMA = climate._CLIMATE_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(FancoilClimate),
-        # User writes a lambda: block; receives `call` (ClimateCall &)
-        cv.Optional(CONF_LAMBDA): cv.returning_lambda,
-    }
+# Inherit the full base CLIMATE_SCHEMA unchanged -- on_control, on_state,
+# visual, etc. are all already defined there. We add nothing extra.
+CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
+    {cv.GenerateID(): cv.declare_id(FancoilClimate)}
 ).extend(cv.COMPONENT_SCHEMA)
 
 
@@ -27,13 +22,3 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
-
-    if CONF_LAMBDA in config:
-        # process_lambda compiles the YAML lambda body into a C++ lambda.
-        # Parameters: [(type, name), ...]  -- `call` available inside the body.
-        control_lambda = await cg.process_lambda(
-            config[CONF_LAMBDA],
-            [(climate_call_ref, "call")],
-            return_type=cg.void,
-        )
-        cg.add(var.set_control_callback(control_lambda))
