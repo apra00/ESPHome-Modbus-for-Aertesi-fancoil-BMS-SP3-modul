2f6c3f8881b4863620c97b86235316f38c7ddf4c
apra00 <76562795+apra00@users.noreply.github.com>
2026-05-18 14:45:14 +0200

    Update climate.py

--- components/fancoil_climate/climate.py
+++ components/fancoil_climate/climate.py
@@ -1,26 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
-from esphome.automation import validate_automation, build_automation
-from esphome.const import CONF_ID, CONF_TRIGGER_ID
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ["climate"]
 AUTO_LOAD = ["climate"]
 
 fancoil_climate_ns = cg.esphome_ns.namespace("fancoil_climate")
 FancoilClimate = fancoil_climate_ns.class_("FancoilClimate", climate.Climate, cg.Component)
-FancoilClimateControlTrigger = fancoil_climate_ns.class_("FancoilClimateControlTrigger")
-
-CONF_ON_CONTROL = "on_control"
 
 CONFIG_SCHEMA = climate._CLIMATE_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(FancoilClimate),
-        cv.Optional(CONF_ON_CONTROL): validate_automation(
-            {
-                cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(FancoilClimateControlTrigger),
-            }
-        ),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -29,11 +20,3 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
-
-    for conf in config.get(CONF_ON_CONTROL, []):
-        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
-        await build_automation(
-            trigger,
-            [(climate.ClimateCall.operator("const").operator("ref"), "x")],
-            conf,
-        )
