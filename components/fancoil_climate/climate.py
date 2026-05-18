f96992c5292171309f44550d56174a95a5ef1914
apra00 <76562795+apra00@users.noreply.github.com>
2026-05-18 12:21:24 +0200

    Create climate.py

---
+++ components/fancoil_climate/climate.py
@@ -0,0 +1,22 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import climate
+from esphome.const import CONF_ID
+
+DEPENDENCIES = ["climate"]
+AUTO_LOAD = ["climate"]
+
+fancoil_climate_ns = cg.esphome_ns.namespace("fancoil_climate")
+FancoilClimate = fancoil_climate_ns.class_("FancoilClimate", climate.Climate, cg.Component)
+
+CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(FancoilClimate),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await climate.register_climate(var, config)
